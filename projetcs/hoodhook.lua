b8bb469bec1efa47dcecd4eb831ace280dbb9d72
CMD-X <65764600+CMD-X@users.noreply.github.com>
2021-01-29 00:30:19 +0000

    Update hoodhook.lua

--- projetcs/hoodhook.lua
+++ projetcs/hoodhook.lua
@@ -6,18 +6,10 @@ mt.__namecall = newcclosure(function(event, ...)
 		local args = {...}
 		local message = args[1]
 		local GangPhrases1 = {"on foenem", "on my momma", "on my granny grave", "watchu talm bout", "watchu say", "oh nawww", "on yo momma", "this reel sh", "yhu wildin kuh", "yhu buggin kuh", "on 4KT", "mic up"}
-		local GangPhrases2 = {["nothing"] = "ntn", ["are"] = "iz", ["too"] = "2", ["bro"] = "jit", ["what"] = "wat", ["whats up"] = "wassup", ["what is up"] = "wazzup", ["no"] = "naw", ["discord"] = "mic up", ["sure"] = "fr", ["crazy"] = "wildin", ["mommy"] = "momma", ["mummy"] = "mumma", ["wilding"] = "wylin", ["dude"] = "kuh", ["friend"] = "kuh", ["yes"] = "yuh", ["about"] = "bout", ["hey"] = "yo", ["lol"] = "yhu funni", ["talking"] = "talm", ["you"] = "yhu", ["do"] = "dhu", ["who"] = "whu", ["blacc"] = "black", ["alright"] = "aight", ["ok"] = "ightt", ["cop"] = "opp", ["police"] = "12", ["house"] = "crib", ["gun"] = "chrome", ["shit"] = "sh", ["weird"] = "trippin", ["gay"] = "sus", ["girl"] = "shordy", ["fire"] = "fie", ["like"] = "lye", ["black"] = "blacc"}
+		local GangPhrases2 = {["nothing"] = "ntn", ["are"] = "iz", ["too"] = "2", ["bro"] = "jit", ["what"] = "wat", ["whats up"] = "wassup", ["what is up"] = "wazzup", ["no"] = "naw", ["discord"] = "mic up", ["sure"] = "fr", ["crazy"] = "wildin", ["mommy"] = "momma", ["mummy"] = "mumma", ["wilding"] = "wylin", ["dude"] = "kuh", ["friend"] = "kuh", ["yes"] = "yuh", ["about"] = "bout", ["hey"] = "yo", ["lol"] = "yhu funni", ["talking"] = "talm", ["you"] = "yhu", ["do"] = "dhu", ["who"] = "whu", ["blacc"] = "black", ["alright"] = "aight", ["ok"] = "ightt", ["cop"] = "opp", ["police"] = "12", ["house"] = "crib", ["gun"] = "chrome", ["shit"] = "sh", ["weird"] = "trippin", ["gay"] = "sus", ["girl"] = "shordy", ["fire"] = "fie", ["like"] = "lye", ["black"] = "blacc", ["tripping"] = "trippin", ["lying"] = "cappin", ["lie"] = "cap"}
 		message = GangPhrases1[math.random(1, #GangPhrases1)].." "..message
-		for i,v in pairs(GangPhrases2) do
-		    message = message:gsub(i,v)
-		end
-		message = message:split(" ")
-		for i,v in pairs(message) do
-		    if v:sub(#v,#v) == "g" then
-		        v = v:sub(1,#v-1)
-		    end
-		end
-		message = table.concat(message, " "):gsub("hoodhoightt", "hoodhook"):upper().."!"
+		for i,v in pairs(GangPhrases2) do message = message:gsub(i,v) end
+		message = message:gsub("hoodhoightt", "hoodhook"):upper().."!"
 		return _G.hoodhook(event, message, args[2])
 	end
 	return _G.hoodhook(event, ...)
